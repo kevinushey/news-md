f8f3a3883fdd301333e784ef1cf7f79c183f96b8
Kevin Ushey <kevinushey@gmail.com>
2015-05-30 00:36:45 -0700

    more work on preamble parsing + normalization

--- parse-news.R
+++ parse-news.R
@@ -24,20 +24,25 @@ strip_whitespace <- function(content) {
   content[begin:end]
 }
 
+normalize <- function(text) {
+  stripped <- sub("^\\s*(.*?)\\s*$", "\\1", text)
+  gsub("\\n\\s*", " ", stripped, perl = TRUE)
+}
+
 extract_ranges <- function(content, reSetext, reAtx) {
   
   setextLocations <- grep(reSetext, content, perl = TRUE)
   setext <- data_frame(
     name = sub(reSetext, "\\1", content[setextLocations], perl = TRUE),
     index = setextLocations,
-    is.setext = if (length(setextLocations)) TRUE
+    is.setext = if (length(setextLocations)) TRUE else logical()
   )
   
   atxLocations <- grep(reAtx, content, perl = TRUE) - 1
   atx <- data_frame(
     name = content[atxLocations],
     index = atxLocations,
-    is.setext = if (length(atxLocations)) FALSE
+    is.setext = if (length(atxLocations)) FALSE else logical()
   )
   
   all <- rbind(setext, atx)
@@ -48,6 +53,7 @@ extract_ranges <- function(content, reSetext, reAtx) {
       tail(sorted$index, n = -1) - 1,
       length(content)
     )
+  else numeric()
   
   sorted
   
@@ -58,18 +64,38 @@ h2_ranges <- function(content) extract_ranges(content, reH2Setext, reH2Atx)
 
 parse_changes <- function(changes) {
   ranges <- h2_ranges(changes)
+  
+  if (!nrow(ranges))
+    ranges <- data_frame(
+      name = "CHANGES",
+      index = 1,
+      begin = 1,
+      end = length(changes)
+    )
+  
   set_names(lapply(seq_len(nrow(ranges)), function(i) {
+    
     bullets <- strip_whitespace(changes[ranges$begin[[i]]:ranges$end[[i]]])
+    
     indices <- grep(reBullet, bullets, perl = TRUE)
     if (!length(indices)) return("")
+    
+    preamble <- if (indices[[1]] > 1)
+      normalize(paste(bullets[seq(to = indices[[1]] - 1)], collapse = "\n"))
+    
     boundaries <- c(indices, length(bullets) + 1)
-    lapply(seq(to = length(boundaries) - 1), function(j) {
+    
+    output <- lapply(seq(to = length(boundaries) - 1), function(j) {
       text <- bullets[seq(from = boundaries[[j]], to = boundaries[[j + 1]] - 1)]
       text[1] <- sub(reBullet, "\\1", text[1], perl = TRUE)
-      pasted <- paste(text, collapse = "\n")
-      normalized <- gsub("\\n\\s+", "\\s", pasted, perl = TRUE)
-      sub("^[\\s\\n]*(.*?)[\\s\\n]*$", "\\1", normalized, perl = TRUE)
+      normalize(paste(text, collapse = "\n"))
     })
+    
+    if (length(preamble))
+      attr(output, "preamble") <- preamble
+    
+    output
+    
   }), ranges$name)
 }
 
@@ -81,9 +107,13 @@ parse_news_md <- function(content) {
   }), ranges$name)
 }
 
+if (!requireNamespace("curl", quietly = TRUE))
+  install.packages("curl")
+
 if (!exists("content")) {
   conn <- curl::curl("https://raw.githubusercontent.com/hadley/dplyr/master/NEWS.md")
   content <- readLines(conn)
+  close(conn)
 }
 
 parsed <- parse_news_md(content)
